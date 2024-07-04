c566c8cd4739500c8c155ad28c2dbe2619b628ae
Mavi <160594705+maviasalemuhmmad@users.noreply.github.com>
2024-07-04 08:49:59 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,84 @@
+// let isRaining = true;
+// if (isRaining){
+//     console.log ("wear a raincoat");
+// }else{
+// console.log("wear sunglasses");
+// }
+// let myAge = 17;
+// if (myAge >= 18){
+//     console.log("you're allowed");
+// }
+// console.log("Water botle not allowed")
+// let month ="Ramadan"
+// if (month=="Ramadan"){
+//     console.log ("Water botle not allowed");
+// }else{
+//     console.log("Water botle is allowed ")
+// }
+// let month="Ramadan"
+// if(month=="jan"){
+//     console.log("Water Botle is allowed")
+// }else{
+//     console.log("Water Botle is not allowed")
+// } if(month == "Ramaden0"){
+//     console.log("Water Botle is not allowed")
+// }else{
+//     console.log("Water botal allowed")
+// }
+// let month="may"
+// console.log(month == "May")
+// if (month == "May"){
+//     console.log("ABC")
+// }
+// let shop="Maida"
+//  console.log(shop !="Maida")
+// let month = "jan"
+// if (month== "june" ){
+// console.log("we not go for picnic");
+// }else  if (month == "out"){
+//     console.log("We not go for picnic");
+// }else {
+// }
+// let month="Ramadan"
+// if(month=="jan"){
+//     console.log("Water Botle is allowed")
+// }else if(month =="remadan"){
+//     console.log("Water Botle is not allowed")
+// } else if (month == "Ramaden0"){
+//     console.log("Water Botle is not allowed")
+// }else{
+//     console.log("Water botal allowed")
+// }
+// let month = "Ramaden" || "Water is 't allowed"
+// console.log(month)
+// let month ="Ramaden" && "Water is't allowed"
+// console.log(month)
+// let month = false || "Water is 't allowed"
+// console.log(month);
+// let tool=false
+// let month= tool && "ups"
+// console.log(month) 
+// let tool="Bulb"
+// let month= tool && "ups"
+// console.log(month)
+// let tool1="Bulb"
+// let tool2="ups"
+// let tool3="No ELECTRICITY"
+// let month= tool1 && tool2 || tool3
+// console.log(month)
+// let tool1="Hot Water"
+// let tool2="Cold WAter"
+// let tool3="No Water"
+// let month= tool1 || (tool2 && tool3)
+// console.log(month)
+// let tool1="Hot Water"
+// let tool2="Cold WAter"
+// let tool3="No Water"
+// let month= tool1 && (tool2 || tool3)
+// console.log(month)
+var tool = "Ramadan main mujy cool water chaie hy ";
+var tool1 = " Agir Shaban hy to mujy hot water chaie hy ";
+var tool2 = "Agir Ramadan hy to mujy cool water chaie hy ";
+var tool3 = "Agir kouch naie hy to mujy no water";
+var month = tool2 && (tool2 || tool3);
+console.log(month);
